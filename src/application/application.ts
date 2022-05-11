dc45ec2b654c60723bfdc2aff84dd5b9ad659f8d
Zainab Salim <zainab.salim@thoughtworks.com>
2022-05-11 11:09:16 +0100

    day 1: problem 3

--- src/application/application.ts
+++ src/application/application.ts
@@ -4,18 +4,13 @@ import { Product } from "../domain/product";
 export class Application {
     static main():void {
         const cart:Cart = new Cart();
-        let product:Product = new Product("Apple Pencil");
+        const product:Product = new Product("Apple Pencil");
+        cart.add(product);
         cart.add(product);
         
         console.log(`Cart = ${cart}`);
         const products:Array<Product> = cart.getProducts();
         
-        console.log("----------------------------------------");
-        console.log(`products = ${products}`);
-        console.log("----------------------------------------");
-        
-        product = new Product("Sony Wireless headphone");
-        cart.add(product);
         
         console.log("----------------------------------------");
         console.log(`products = ${products}`);
